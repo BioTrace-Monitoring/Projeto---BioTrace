4be2735bac1229ed71befddc673b6d9e7c41e545
brxnogoncalves <bruno.goncalves@sptech.school>
2026-09-22 21:46:39 -0300

    feat: separa login da busca de permissões

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -11,7 +11,6 @@ var usuarioController = require("../controllers/usuarioController");
 
 
 
-
 // Rota POST para cadastrar um usuário
 // POST para enviar dados ao servidor
 router.post("/cadastrar-usuario", function (req, res)
@@ -26,41 +25,13 @@ router.post("/cadastrar-usuario", function (req, res)
 })
 
 
-
-
-// Rota POST para autenticar um usuário
-// POST para enviar dados ao servidor
-router.post("/autenticar-usuario", function (req, res)
-{
-    // Chamando a função autenticarUsuario do usuarioController.js
-
-    // req -> requisição: Possui todas as informações da requisição
-    // res -> resposta: Retornar uma resposta pro usuario
-
-    // Direcionando a requisição pro controller responsavel
+router.post("/autenticar", function (req, res) {
     usuarioController.autenticarUsuario(req, res);
 });
 
-
-
-
-// ROTA GET para visualizar os dados um usuário
-// GET para buscar dados do servidor
-router.get("/visualizar-usuario/:fkUsuario", function (req, res)
-{
-    // Chamando a função visualizarUsuario do usuarioController.js
-
-    // :fkUsuario -> parâmetro de rota, o valor será dinâmico dependendo do usuario logado
-
-    // req -> requisição: Possui todas as informações da requisição
-    // res -> resposta: Retornar uma resposta pro usuario
-
-    // Direcionando a requisição pro controller responsavel
-    usuarioController.visualizarUsuario(req, res);
-})
-
-
-
+router.get("/permissoes/:idUsuario", function (req, res) {
+    usuarioController.obterPermissoesUsuario(req, res);
+});
 
 // Exportando o router
 // Outros arquivos podem usar essas funções
